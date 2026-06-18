15dc4db65a69a919d6b66672c5fc52eca23c0fcc
Henrique <henriquebignatti@gmail.com>
2026-06-18 12:36:17 -0300

    Isolate public share route from app bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "@blocknote/core/fonts/inter.css";
 import "@blocknote/mantine/style.css";
 import './styles/tokens.css';
 import './index.css';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import AuthProvider, { useAuth } from "./contexts/AuthContext";
 import TaskMenuContext from "./contexts/TaskMenuContext";
 import { signUpAction } from "./components/forms/signUpAction.js";
@@ -12,10 +12,10 @@ import { resetPasswordAction } from "./components/forms/resetPasswordAction.js";
 import { updateUserAction } from "./components/forms/updateUserAction.js";
 import Error from "./components/Error.jsx";
 import BrandedLoadingIndicator from "./components/BrandedLoadingIndicator.jsx";
+import PublicSharePage from "./PublicSharePage.jsx";
 
 const HomePage = React.lazy(() => import("./HomePage"));
 const NotFound = React.lazy(() => import("./components/NotFound"));
-const PublicSharePage = React.lazy(() => import("./PublicSharePage.jsx"));
 
 function LazyPage({ children }) {
     return (
@@ -58,7 +58,7 @@ function App() {
             />
             <Route
                 path="/share/:shareToken"
-                element={<LazyPage><PublicSharePage /></LazyPage>}
+                element={<PublicSharePage />}
                 errorElement={<Error />}
             />
             <Route
@@ -75,10 +75,28 @@ function App() {
     return <RouterProvider router={router} />;
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <AuthProvider>
-        <TaskMenuContext>
-            <App />
-        </TaskMenuContext>
-    </AuthProvider>
-);
+function PublicShareApp() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/share/:shareToken" element={<PublicSharePage />} />
+                <Route path="*" element={<LazyPage><NotFound /></LazyPage>} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const isPublicShareRoute = typeof window !== "undefined" && /^\/share\/[^/]+/.test(window.location.pathname);
+
+if (isPublicShareRoute) {
+    root.render(<PublicShareApp />);
+} else {
+    root.render(
+        <AuthProvider>
+            <TaskMenuContext>
+                <App />
+            </TaskMenuContext>
+        </AuthProvider>
+    );
+}
